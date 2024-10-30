e7b572275f5afdd18563d5150f5ff5cb54596a54
Manuel Tello Siri <manuelTello.business@gmail.com>
2024-10-30 06:35:38 -0700

    Add project files.

---
+++ Veterinary.Slices.Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Veterinary.Slices.Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
